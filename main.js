e389f5e4b8ef904bea66fc327010b8c78034ea12
JavRomero89 <javieromero23@gmail.com>
2023-07-03 17:44:20 +0200

    advance

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-let myArray = [];
+let Array = [];
 
-myArray.push("valor1");
-myArray.push("valor2");
+Array.push("valor1");
+Array.push("valor2");
 
-myArray.unshift("valor3");
-myArray.unshift("valor4");
+Array.unshift("valor3");
+Array.unshift("valor4");
 
-myArray.pop();
-myArray.shift();
+Array.pop();
+Array.shift();
 
-console.log("Longitud del array:", myArray.length);
+console.log("Longitud del array:", Array.length);
 
 console.log("-------------------------------")
 
@@ -44,4 +44,41 @@ const lettersArray = sentence.split("");
 console.log(lettersArray); 
 
 console.log("-------------------------------")
+
+const months = ["January", "February", "Marj", "April", "Mei"];
+months[2]= "March"
+months[4]= "May"
+
+console.log(months);
+
+const newMonths = ["June", "July", "August", "September"];
+const totalMonths = months.concat(newMonths);
+
+console.log(totalMonths);
+
+totalMonths.push("October", "November", "December");
+
+console.log(totalMonths);
+
+console.log("-------------------------------")
+
+let num1 = 9;
+let num2 = 7;
+let num3 = 5;
+
+if (num1 > num2) {
+  totalMonths.pop(); // Elimina el último elemento del array
+} else if (num2 > num3 && num2 < 10) {
+  totalMonths.shift(); // Elimina el primer elemento del array
+} else if (num2 > num3 || num2 > num1) {
+  totalMonths.push("October"); // Añade "October" al final del array
+} else {
+  console.log("Bad luck!"); // Ninguna condición se cumple
+}
+
+console.log(totalMonths);
+
+
+
+
 
